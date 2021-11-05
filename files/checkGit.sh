ad2c5fdb4c65121be83d412185815a638e033948
aselvan-pa <79080465+aselvan-pa@users.noreply.github.com>
2021-11-05 18:15:51 +0100

    Update checkGit.sh

--- files/checkGit.sh
+++ files/checkGit.sh
@@ -12,4 +12,11 @@ REPOOUTPUT=$(curl -k -u $TL_USER:$TL_PASS \
 VULN=$(echo $REPOOUTPUT | sed -n -e 's/^.*\(vulnerabilitiesCount\)/\1/p' | cut -f1 -d, | cut -f2- -d:)
 #VULN=$(cat output.txt)
 
+if (( $VULN == 0 )); then
+   echo "No Code Repo Vulnerabilities!"
+   exit 0
+else
+   echo "There are $VULN vulnerabilities on dependencies in Git"
+   exit 1
+fi
 
